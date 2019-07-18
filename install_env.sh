f2746a2df4161f242468fbc79a44e97922dbf7a8
unknown <lijiacai@pand-auto.com>
2019-07-18 17:40:50 +0800

    mf

--- install_env.sh
+++ install_env.sh
@@ -1,7 +1,13 @@
 yum -y install wget
-
+yum -y install git
 wget https://nodejs.org/dist/latest-v7.x/node-v7.10.1.tar.gz
 tar -xvzf node-v7.10.1.tar.gz
-cd ode-v7.10.1
+cd node-v7.10.1
 ./configure
 make && make install
+
+cd -
+rm -rf node-v7.10.1
+rm -rf node-v7.10.1.tar.gz
+
+git clone https://github.com/o1lab/xmysql.git
